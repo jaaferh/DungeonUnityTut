2f98276e5ac1d7a234c0fdaac6ae9d4de4532536
Jaafer <jaaferluay@gmail.com>
2021-12-15 11:08:22 +0000

    Add Mover script and enemy movement logic

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,7 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : Fighter
+public class Enemy : Mover
 {
-    
+    // Experience
+    public int xpValue = 1;
+
+    // Logic
+    public float triggerLength = 1; // distance for enemy to start chasing
+    public float chaseLength = 5; // how far will enemy chase for
+    private bool chasing;
+    private bool collidingWithPlayer;
+    private Transform playerTransform; // movement of the player
+    private Vector3 startingPosition;
+
+    // Hitbox
+    public ContactFilter2D filter;
+    private BoxCollider2D hitbox;
+    private Collider2D[] hits = new Collider2D[10]; // duplicate from Collidable.cs
+
+
+    protected override void Start()
+    {
+        base.Start();
+        playerTransform = GameManager.instance.player.transform; 
+        startingPosition = transform.position; // current position of this object on launch
+        hitbox = transform.GetChild(0).GetComponent<BoxCollider2D>(); // gets first child which is the hitbox in this case
+    }
+
+    private void FixedUpdate()
+    {
+        // Is the player in range?
+        if (Vector3.Distance(playerTransform.position, startingPosition) < chaseLength)
+        {
+            if (Vector3.Distance(playerTransform.position, startingPosition) < triggerLength)
+                chasing = true;
+
+            if (chasing)
+            {
+                if (!collidingWithPlayer)
+                {
+                    Vector3 towardsPlayer = (playerTransform.position - transform.position).normalized; // normalises a vector to have a direction from enemy towards player position
+                    UpdateMotor(towardsPlayer);
+                }
+            }
+            else
+            {
+                Vector3 towardsStart = startingPosition - transform.position; // towards start position
+                UpdateMotor(towardsStart);
+            }
+        }
+        else
+        {
+            UpdateMotor(startingPosition - transform.position); // towards start position
+            chasing = false;
+        }
+
+
+        // Check for overlaps
+        collidingWithPlayer = false;
+        boxCollider.OverlapCollider(filter, hits); // returns everything colliding with this boxCollider, filtered by filter
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if (hits[i] == null)
+                continue;
+
+            // Do something on collide (hit)
+            if (hits[i].CompareTag("Fighter") && hits[i].name == "Player")
+                collidingWithPlayer = true;
+
+            // The array is not cleaned up, so we do it ourself
+            hits[i] = null;
+        }
+    }
+
+    protected override void Death()
+    {
+        Destroy(gameObject);
+        GameManager.instance.experience += xpValue;
+        GameManager.instance.ShowText("+" + xpValue + " xp", 30, Color.magenta, transform.position, Vector3.up * 40, 1.0f);
+    }
 }
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Mover : Fighter
+{
+    protected BoxCollider2D boxCollider;
+    protected Vector3 moveDelta;
+    protected RaycastHit2D hit;
+    protected float ySpeed = 0.75f;
+    protected float xSpeed = 1.0f;
+
+    protected virtual void Start() 
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    protected virtual void UpdateMotor(Vector3 input)
+    {
+        // Reset moveDelta
+        moveDelta = new Vector3(input.x * xSpeed, input.y * ySpeed, 0);
+
+        // Swap sprite direction, whether youre going right or left
+        if (moveDelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if (moveDelta.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        // Can move in this direction by casting a box there first. If box == null, we can move
+        // y axis
+        hit = Physics2D.BoxCast(
+            transform.position, 
+            boxCollider.size, 
+            0, 
+            new Vector2(0, moveDelta.y), 
+            Mathf.Abs(moveDelta.y * Time.deltaTime), 
+            LayerMask.GetMask("Actor", "Blocking")
+        );
+        if (hit.collider == null)
+        {
+            // Make player move
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+
+        // x axis
+        hit = Physics2D.BoxCast(
+            transform.position, 
+            boxCollider.size, 
+            0, 
+            new Vector2(moveDelta.x, 0), 
+            Mathf.Abs(moveDelta.x * Time.deltaTime), 
+            LayerMask.GetMask("Actor", "Blocking")
+        );
+        if (hit.collider == null)
+        {
+            // Make player move
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
+    }
+}
