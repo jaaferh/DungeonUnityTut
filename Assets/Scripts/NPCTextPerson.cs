cfdb35bd61ee270a0f13becc99d1972a4cfa22b2
Jaafer <jaaferluay@gmail.com>
2021-12-29 12:11:47 +0000

    Added choices and file saving

--- Assets/Scripts/NPCTextPerson.cs
+++ Assets/Scripts/NPCTextPerson.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class NPCTextPerson : Collidable
@@ -26,16 +27,38 @@ public class NPCTextPerson : Collidable
         public string answer2;
     }
 
+    // References
     public TextAsset npcJson;
-    public Dialogue[] dialogue;
-    public string[] messages;
     public NPCDialogue npcDialogue;
+    public Dialogue[] dialogue;
+
+    // Logic
+    [SerializeField]
+    private Dictionary<string, bool> choicesChosen = new Dictionary<string, bool>();
+    private string savePath; 
+
+    protected virtual void Awake()
+    {
+        savePath = Application.persistentDataPath + "/" + this.name + ".json";
+    }
 
     protected override void Start()
     {
         base.Start();
 
         var dlgReqJson2 = JSONSerializer.Deserialize(typeof(DialogueReqColl), npcJson.text);
+
+
+        // Get Choices from saved file
+        string choicesJson = File.ReadAllText(savePath);
+        choicesChosen = (Dictionary<string, bool>)JSONSerializer.Deserialize(typeof(Dictionary<string, bool>), choicesJson);
+
+        if (choicesChosen.Keys.Count < 1)
+            choicesChosen.Add("spoken", true);
+        
+        // Save Choices to saved file
+        string serializedChoices = JSONSerializer.Serialize(typeof(Dictionary<string, bool>), choicesChosen);
+        File.WriteAllText(savePath, serializedChoices);
     }
     protected override void OnCollide(Collider2D coll)
     {
